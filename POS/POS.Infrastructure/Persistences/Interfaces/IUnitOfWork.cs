ca47292f9ad29c0876cd6887ba293eb66372c89a
CoreDX1 <chrismaquis@gmail.com>
2023-01-19 19:07:09 -0300

    Creado la interface de Unit Of Work

---
+++ POS/POS.Infrastructure/Persistences/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+
+namespace POS.Infrastructure.Persistences.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+        // Declaracion o matriculas de nuestra interface a nivel de repositorio
+        // Todas las declaraciones de las interface a nivel de repositorio
+        IUserRepository User { get; }
+        void SaveChanges();
+        Task SaveChangesAsync();
+}
