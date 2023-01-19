1c0d6e01885fcdd98cb5dedb65039f66e9bae52f
CoreDX1 <chrismaquis@gmail.com>
2023-01-19 18:27:52 -0300

    Se creo un nuevo archivo

--- POS/POS.Infrastructure/Persistences/Context/FormContext.cs
+++ POS/POS.Infrastructure/Persistences/Context/FormContext.cs
@@ -26,42 +26,6 @@ public partial class FormContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity.HasKey(e => e.UserId).HasName("PK_user");
-
-            entity.ToTable("User");
-
-            entity.HasIndex(e => e.Email, "UQ__User__AB6E6164495D00C4").IsUnique();
-
-            entity.Property(e => e.UserId).HasColumnName("userId");
-            entity.Property(e => e.DateRegister)
-                .HasDefaultValueSql("(getdate())")
-                .HasColumnType("datetime")
-                .HasColumnName("dateRegister");
-            entity.Property(e => e.Email)
-                .HasMaxLength(100)
-                .IsUnicode(false)
-                .HasColumnName("email");
-            entity.Property(e => e.LastName)
-                .HasMaxLength(50)
-                .IsUnicode(false)
-                .HasColumnName("lastName");
-            entity.Property(e => e.Name)
-                .HasMaxLength(50)
-                .IsUnicode(false)
-                .HasColumnName("name");
-            entity.Property(e => e.RolId).HasColumnName("rolId");
-            entity.Property(e => e.Status)
-                .HasDefaultValueSql("((1))")
-                .HasColumnName("status");
-
-            entity.HasOne(d => d.Rol).WithMany(p => p.Users)
-                .HasForeignKey(d => d.RolId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__User__rolId__3E52440B");
-        });
-
         OnModelCreatingPartial(modelBuilder);
     }
 
