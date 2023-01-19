4be556a0d29803b100baed04dd06dd36484b916a
CoreDX1 <chrismaquis@gmail.com>
2023-01-19 18:45:09 -0300

    Documentado

--- POS/POS.Infrastructure/Extensions/InjectionExtensions.cs
+++ POS/POS.Infrastructure/Extensions/InjectionExtensions.cs
@@ -6,8 +6,18 @@ using POS.Infrastructure.Persistences.Context;
 namespace POS.Infrastructure.Extensions;
 
 // configurar la conexion
+/// <summary>
+/// Extension method for configuring the database connection
+/// </summary>
 public static class InjectionExtensions
 {
+    /// <summary>
+    /// Adds the FormContext to the service collection and configures it to use SQL Server with a specified connection string.
+    /// Also specifies the assembly containing the migrations.
+    /// </summary>
+    /// <param name="service">The service collection to add the FormContext to</param>
+    /// <param name="configuration">The configuration object containing the connection string</param>
+    /// <returns>The updated service collection</returns>
     public static IServiceCollection AddInjectionInfrastructure(
         this IServiceCollection service,
         IConfiguration configuration
